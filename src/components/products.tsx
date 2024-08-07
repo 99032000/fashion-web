0f38ff0279487485e79866792c339039324b9841
damon_gege <z3439119@gmail.com>
2024-08-08 01:38:57 +1000

    fix bug

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -13,7 +13,7 @@ export function Products({ data }: Readonly<Props>) {
   return (
     <div className="w mx-auto max-w-content px-16 pt-14">
       <Title title={data.title} subtitle={data.subtitle} />
-      <div className="mb-8 grid grid-cols-4 gap-x-4 gap-y-16">
+      <div className="mb-8 grid grid-cols-4 gap-x-6 gap-y-16">
         {data.products.map((item) => (
           <CardView key={item.id} productData={item} isProduct />
         ))}
